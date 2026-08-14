22bd63473c13c07b1045c6952cefd1e7136c5373
rootankush <rootankushprompt@gmail.com>
2026-08-14 12:57:13 +0530

    chore: add initial resume boilerplate information

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+const initialPersonalDetails = {
+	firstName: "John",
+	lastName: "Doe",
+	profession: "Software Developer",
+	phoneNumber: "(555) 123-4567",
+	email: "john.doe@example.com",
+};
+
+const initialEducationDetails = {
+	collageName: "State University",
+	degree: "Bachelor of Science in Computer Science",
+	startDate: "08/2018",
+	endDate: "05/2022",
+	location: "Springfield, IL",
+};
+
+const initialSkillsDetails = {
+	skills: "HTML CSS Javascript",
+};
+
+const initialWorkDetails = {
+	// companyName: "Northstar Labs",
+	jobTitle: "Software Engineer",
+	startDate: "06/2022",
+	endDate: "08/2025",
+	responsibilities:
+		"Developed and maintained customer-facing web application features.",
+};
+
+export {
+	initialPersonalDetails,
+	initialEducationDetails,
+	initialSkillsDetails,
+	initialWorkDetails,
+};
